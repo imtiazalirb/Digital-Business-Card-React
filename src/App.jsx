0516990f7e4ff5bfcc9db8cfc4f6213f7a4ba894
Imtiaz Ali <imtiazali4090@gmail.com>
2022-08-31 18:56:21 +0600

    feat: about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Info from './components/Info'
+import About from './components/About'
 import './App.css'
 
 export default function App() {
   return (
     <div className="App">
       <Info />
+      <About />
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function About () {
+    return(
+        <div className="about--section">
+            <h2>About</h2>
+            <p>
+                I am a gradute from American International University-Bangldesh in Computer Science Engineering. currently looking forward to join as a frontend software software engineer,
+                I am interest in to join fast grown startups who priotizes slicks designs to deliver best possible UX for their customers.  
+            </p>
+        </div>
+    )
+}
+
+export default About
